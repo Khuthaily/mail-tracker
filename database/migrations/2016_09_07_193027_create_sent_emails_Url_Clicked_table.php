a4e63a9b5998c195a372232a0a200cc362338c59
Khaled Huthaily <khaledenglish@yahoo.com>
2023-12-26 10:03:54 -0700

    use varchar instead of text for url column
    use varchar instead of text for url column

--- database/migrations/2016_09_07_193027_create_sent_emails_Url_Clicked_table.php
+++ database/migrations/2016_09_07_193027_create_sent_emails_Url_Clicked_table.php
@@ -3,35 +3,25 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use jdavidbakr\MailTracker\MailTracker;
 
 class CreateSentEmailsUrlClickedTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up(): void
     {
-        Schema::connection(MailTracker::sentEmailUrlClickedModel()->getConnectionName())->create('sent_emails_url_clicked', function (Blueprint $table) {
+        Schema::create('sent_emails_url_clicked', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('sent_email_id')->unsigned();
             $table->foreign('sent_email_id')->references('id')->on('sent_emails')->onDelete('cascade');
-            $table->text('url')->nullable();
+            $table->string('url', 2083)->nullable()->index();
             $table->char('hash', 32);
-            $table->integer('clicks')->default('1');
-            $table->timestamps();
+            $table->integer('clicks')->default(1);
+            $table->dateTime('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->dateTime('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'))->onUpdate(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
-        Schema::connection(MailTracker::sentEmailUrlClickedModel()->getConnectionName())->drop('sent_emails_url_clicked');
+        Schema::dropIfExists('sent_emails_url_clicked');
     }
-}
+};
