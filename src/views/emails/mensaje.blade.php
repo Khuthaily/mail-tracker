409818817cdb562176ca27277dbf61ffd585eb25
Khaled Huthaily <khaledenglish@yahoo.com>
2024-02-16 13:22:08 -0700

    use config('app.url') in mensaje.blade.php
    Use `config('app.url')` instead of `env('APP_URL')` to avoid returning `null` if user cached `config`.

--- src/views/emails/mensaje.blade.php
+++ src/views/emails/mensaje.blade.php
@@ -17,13 +17,13 @@
    {{ $data['message'] }}
 @endsection
 @section('href_call_to_action')
-    {{env('APP_URL')}}
+    {{config('app.url')}}
 @endsection
 @section('txt_call_to_action')
     Call To Action
 @endsection
 @section('txt_extra')
-    This email comes from <a href="{{env('APP_URL')}}" style="text-decoration:none; color:#4b679d;">{{config('mail-tracker.name')}}</a>
+    This email comes from <a href="{{config('app.url')}}" style="text-decoration:none; color:#4b679d;">{{config('mail-tracker.name')}}</a>
 @endsection
 @section('saludo_final')
     Regards
